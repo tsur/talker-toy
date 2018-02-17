0f549d4ac853121e7b16bfa0480b108bfa5ebb61
Tsur <prucheta@gmail.com>
2018-02-17 18:40:24 +0100

    feat(scripts): bluetooth reconnect

--- scripts/bluetooth_autopair.sh
+++ scripts/bluetooth_autopair.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 bluetoothctl << EOF
 connect 54:E2:56:8B:EB:1A
+EOF
 #connect <bt_device_mac_address>
 #using aqua bluetooth speaker mac address, el altavoz rojo redondo
-EOF
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get -y install screen git
 sudo apt-get -y install python-pip python-dev
-sudo apt-get -y install alsa-tools alsa-utils portaudio19-dev
+sudo apt-get -y install alsa-tools alsa-utils portaudio19-dev pulseaudio
 sudo apt-get -y install espeak python-espeak
 sudo apt-get -y install python-requests python-imaging python-imaging-tk
 # if running python3
@@ -13,3 +13,11 @@ sudo pip install -r requirements.txt
 sudo pip install awscli
 sudo aws configure
 
+# When using mini jack 3.5, run this
+# amixer cset numid=3 1 # force headphone (PWM) output
+# and make sure the line dtparam=audio=on in file /boot/config.txt is uncomment
+
+# To use bluetooth https://raspberrypi.stackexchange.com/questions/53408/automatically-connect-trusted-bluetooth-speaker
+# echo 'pulseaudio --start' >> ~/.bashrc
+# echo 'wait' >> ~/.bashrc
+# echo '/home/py/talker-toy/scripts/bluetooth_reconnect.py' >> ~/.bashrc
