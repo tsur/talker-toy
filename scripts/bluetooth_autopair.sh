782bb3492b3d22a1556767a5c103586a6f4621ae
Tsur <prucheta@gmail.com>
2018-02-17 23:37:42 +0100

    feat(pacmd): set default sink kill pulseaudio

--- scripts/bluetooth_autopair.sh
+++ scripts/bluetooth_autopair.sh
@@ -4,4 +4,5 @@ connect 54:E2:56:8B:EB:1A
 EOF
 #connect <bt_device_mac_address>
 #using aqua bluetooth speaker mac address, el altavoz rojo redondo
+echo "Default sink"
 pacmd set-default-sink bluez_sink.54_E2_56_8B_EB_1A.a2dp_sink
