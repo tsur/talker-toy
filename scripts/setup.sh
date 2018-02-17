84be0b4ac18310c800f184b5597d3d61a7c01e48
Tsur <prucheta@gmail.com>
2018-02-17 18:30:19 +0100

    feat(scripts): bluetooth autopair

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get -y install screen git
 sudo apt-get -y install python-pip python-dev
-sudo apt-get -y install alsa-tools alsa-utils
+sudo apt-get -y install alsa-tools alsa-utils portaudio19-dev
 sudo apt-get -y install espeak python-espeak
 sudo apt-get -y install python-requests python-imaging python-imaging-tk
 # if running python3
@@ -10,6 +10,6 @@ sudo apt-get -y install python-requests python-imaging python-imaging-tk
 sudo pip install --upgrade pip
 sudo pip install -r requirements.txt
 # Remember to configure aws 
-# sudo pip install awsscli
-# sudo aws configure
+sudo pip install awscli
+sudo aws configure
 
